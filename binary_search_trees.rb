afe21665ea1b8e23f75a12fd436bf8ec0a9b089e
Tygh Walters <tyghwalters@gmail.com>
2022-10-31 18:16:51 -0700

    The delete_rec method handles nodes with 2 children

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -1,5 +1,4 @@
 class Node
-
   attr_accessor :data, :right, :left
 
   def initialize(data = nil, left = nil, right = nil)
@@ -10,9 +9,7 @@ class Node
 
 end
 
-
 class Tree
-
   attr_accessor :root
 
   def initialize(array)
@@ -35,7 +32,7 @@ class Tree
     root
   end
 
-  # pretty_print method from TOP student
+  # pretty_print method from a TOP student
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -43,7 +40,7 @@ class Tree
   end
 
   def insert_rec(value, root = @root)
-    if root == nil # base case
+    if root.nil? # base case
       new_node = Node.new(value)
       return new_node
     elsif value == root.data
@@ -56,22 +53,40 @@ class Tree
     root
   end
 
-  def delete(value)
-    # 1- Node to be deleted is the leaf: Simply remove from the tree.
-    # find node and set to nil
-    
-    # 2- Node to be deleted has 1 child: 
-    # - Copy child to the node & delete child
+  def delete_rec(value, root = @root)
+    return if root.nil? # base case nothing found
 
-    # 3- Node to be deleted has 2 children:
-    # - Find inorder successor of the node. 
-    # - Copy contents of the inorder successor to the node and 
-    # - delete the inorder successor.
-    # - inorder successor can be obtained by 
-    # finding the minimum value in the right child of the node.
-
+    if value < root.data
+      root.left = delete_rec(value, root.left)
+    elsif value > root.data
+      root.right = delete_rec(value, root.right)
+    else # the node matches, so delete
+      # node has 0 or only right child
+      return root.right if root.left.nil?
+      # node has only left child
+      return root.left if root.right.nil?
+      
+      # Node to be deleted has 2 children:
+        # - Find inorder successor of the node.
+        succ_node = min_node(root.right)
+        root.data = succ_node.data
+        # - Copy contents of the inorder successor to the node and 
+        # - delete the inorder successor.
+        root.right = delete_rec(root.data, root.right)
+        # - inorder successor can be obtained by 
+        # finding the minimum value in the right child of the node.
+    end  
+      root
   end
 
+  def min_node(root)
+    min_root = root
+    while !root.left.nil? do # there's smaller in the left?
+      min_root = root.left # update with a smaller value
+      root = root.left # assigning new root, to keep searching
+    end
+    min_root
+  end
 
 end
 
@@ -79,7 +94,7 @@ array = [6345, 1, 14, 44, 23, 8, 0, 3, 50, 7, 9, 67, 324]
 
 bst = Tree.new(array)
 bst.pretty_print
-bst.insert_rec(65)
+bst.insert_rec(500)
 bst.pretty_print
-# bst.delete(13)
-# bst.pretty_print
+bst.delete_rec(50)
+bst.pretty_print
